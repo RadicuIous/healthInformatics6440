b83fa3873acddf0ae4d8c9763a77373cd3202db6
krashoverride <wr25@njit.edu>
2015-03-02 20:50:27 -0500

    fix for overview

--- js/controllers/OverviewCtrl.js
+++ js/controllers/OverviewCtrl.js
@@ -15,6 +15,12 @@ angular.module('myApp.controllers').controller('OverviewCtrl', [
         var observationSuccessApiCall = function(successResponse) {
             $log.info("Response received by MainCtrl.js :: observationSuccessApiCall");
             $scope.observationObject = successResponse;
+            $scope.patientStats = {};
+            for (var i = 0; i < $scope.observationObject.entry.length; i++){
+                if( !($scope.patientStats.hasOwnProperty($scope.observationObject.entry[i].content.name.coding[0].display))){
+                    $scope.patientStats[$scope.observationObject.entry[i].content.name.coding[0].display] = $scope.observationObject.entry[i];
+                }
+            }
         }
         var observationFailureApiCall = function(failureResponse) {
             $log.info("Response received by MainCtrl.js :: observationFailureApiCall");
