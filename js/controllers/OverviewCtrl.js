07f23746b11888a422dff7efc9f5220ab366de60
krashoverride <wr25@njit.edu>
2015-03-26 17:59:28 -0400

    fix for overview

--- js/controllers/OverviewCtrl.js
+++ js/controllers/OverviewCtrl.js
@@ -16,9 +16,25 @@ angular.module('myApp.controllers').controller('OverviewCtrl', [
             $log.info("Response received by MainCtrl.js :: observationSuccessApiCall");
             $scope.observationObject = successResponse;
             $scope.patientStats = {};
+            $scope.uniqueObservations = {};
+            $scope.observationHistory = {};
+            var healthOverview = [
+                "Body Height",
+                "Body Weight",
+                "Diastolic BP",
+                "Systolic BP",
+                "Heart Beat",
+                "Respiration Rate",
+                "Total Cholesterol"];
             for (var i = 0; i < $scope.observationObject.entry.length; i++){
                 if( !($scope.patientStats.hasOwnProperty($scope.observationObject.entry[i].content.name.coding[0].display))){
-                    $scope.patientStats[$scope.observationObject.entry[i].content.name.coding[0].display] = $scope.observationObject.entry[i];
+                    if(healthOverview.indexOf($scope.observationObject.entry[i].content.name.coding[0].display) > -1){
+                        $scope.patientStats[$scope.observationObject.entry[i].content.name.coding[0].display] = $scope.observationObject.entry[i];
+                    }else{
+                        $scope.uniqueObservations[$scope.observationObject.entry[i].content.name.coding[0].display] = $scope.observationObject.entry[i];
+                    }
+                }else{
+                    $scope.observationHistory[$scope.observationObject.entry[i].content.name.coding[0].display] = $scope.observationObject.entry[i];
                 }
             }
         }
