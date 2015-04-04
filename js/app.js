81207ea7835d107a85d694a2e244b4ef1ac58dee
michaelbranin <mbranin@gmail.com>
2015-04-04 19:51:49 -0400

    Added Medications

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/Index', {templateUrl: 'partials/index.html', controller: 'MainCtrl'});
     $routeProvider.when('/Login', {templateUrl: 'partials/login.html', controller: 'LoginCtrl'});
     $routeProvider.when('/Overview/:patientId', {templateUrl: 'partials/overview.html', controller: 'OverviewCtrl'});
-    $routeProvider.when('/Graphs/:patientId', {templateUrl: 'partials/graphs.html', controller: 'GraphCtrl'});
+    $routeProvider.when('/Medications/:patientId', {templateUrl: 'partials/medications.html', controller: 'MedicationsCtrl'});
     $routeProvider.when('/History/:patientId', {templateUrl: 'partials/history.html', controller: 'HistoryCtrl'});
     $routeProvider.when('/Management/:patientId', {templateUrl: 'partials/management.html', controller: 'ManagementCtrl'});
     $routeProvider.when('/Doctors/:patientId', {templateUrl: 'partials/doctors.html', controller: 'DoctorsCtrl'});
--- js/controllers/GraphCtrl.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('myApp.controllers').controller('GraphCtrl', ['$scope','$rootScope','NavBarService', function ($scope, $rootScope, NavBarService) {
-    $rootScope.showNavBar = true;
-    NavBarService.updateNavigation('GRAPHS');
-}]);
