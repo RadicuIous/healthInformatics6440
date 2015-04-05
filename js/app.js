41e99e7bd9fff696e3203a85b6ee77221495c2c1
michaelbranin <mbranin@gmail.com>
2015-04-04 20:03:33 -0400

    Merge branch 'master' of https://github.com/krashoverride/healthInformatics6440
    Conflicts:
    	index.html
    	js/app.js

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ angular.module('myApp', [
     'ngRoute',
     'ngAnimate',
     'myApp.controllers',
+    'myApp.filters',
 	'healthInformatics.fhir.api',
     'healthInformatics.fhir.navbar'
 ]).
@@ -13,7 +14,7 @@ config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/Login', {templateUrl: 'partials/login.html', controller: 'LoginCtrl'});
     $routeProvider.when('/Overview/:patientId', {templateUrl: 'partials/overview.html', controller: 'OverviewCtrl'});
     $routeProvider.when('/Medications/:patientId', {templateUrl: 'partials/medications.html', controller: 'MedicationsCtrl'});
-    $routeProvider.when('/History/:patientId', {templateUrl: 'partials/history.html', controller: 'HistoryCtrl'});
+    $routeProvider.when('/Condition/:patientId', {templateUrl: 'partials/condition.html', controller: 'ConditionCtrl'});
     $routeProvider.when('/Management/:patientId', {templateUrl: 'partials/management.html', controller: 'ManagementCtrl'});
     $routeProvider.when('/Doctors/:patientId', {templateUrl: 'partials/doctors.html', controller: 'DoctorsCtrl'});
     $routeProvider.otherwise({redirectTo: '/Index'});
